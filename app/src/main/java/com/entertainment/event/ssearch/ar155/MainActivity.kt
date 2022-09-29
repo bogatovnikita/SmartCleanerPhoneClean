41d058e520082f8e0331bff3761bc4f779379a4b
dimap <dima.pasyutin2@gmail.com>
2022-09-29 16:16:38 +0300

    init

---
+++ app/src/main/java/com/entertainment/event/ssearch/ar155/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.entertainment.event.ssearch.ar155
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
