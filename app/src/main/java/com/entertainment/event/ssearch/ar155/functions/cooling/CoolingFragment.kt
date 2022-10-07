d13eee2c65f67e0d5d443d2542755248676dc821
dimap <dima.pasyutin2@gmail.com>
2022-10-07 04:57:48 +0300

    made cooling layout

---
+++ app/src/main/java/com/entertainment/event/ssearch/ar155/functions/cooling/CoolingFragment.kt
@@ -0,0 +1,12 @@
+package com.entertainment.event.ssearch.ar155.functions.cooling
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.entertainment.event.ssearch.ar155.R
+import com.entertainment.event.ssearch.ar155.databinding.FragmentCoolingBinding
+
+class CoolingFragment : Fragment(R.layout.fragment_cooling) {
+
+    private val binding: FragmentCoolingBinding by viewBinding()
+
+}
