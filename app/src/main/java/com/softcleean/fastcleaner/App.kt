912240e7add83ac48ff6d1991d038d4f6a03f907
dimap <dima.pasyutin2@gmail.com>
2023-01-18 11:18:57 +0300

    first fix analytics

--- app/src/main/java/com/softcleean/fastcleaner/App.kt
+++ app/src/main/java/com/softcleean/fastcleaner/App.kt
@@ -3,7 +3,14 @@ package com.softcleean.fastcleaner
 import android.app.ActivityManager
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
 import android.os.Process
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import com.softcleean.fastcleaner.data.shared_pref.UtilsProviderForCLibrary
 import com.yandex.metrica.YandexMetrica
 import com.yandex.metrica.YandexMetricaConfig
@@ -19,6 +26,20 @@ class App: Application() {
         YandexMetrica.enableActivityAutoTracking(this)
         if (isMainProcess()) {
             UtilsProviderForCLibrary.initUtilsProviderForCLibrary(this)
+        }
+        FirebaseApp.initializeApp(this)
+        FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(true) // 30 and 31 save
+        FirebaseAnalytics.getInstance(this).logEvent(
+            "test",
+            Bundle()
+        )
+        val settings = remoteConfigSettings {
+            this.minimumFetchIntervalInSeconds = 1
+        }
+        Firebase.remoteConfig.setConfigSettingsAsync(settings)
+        Firebase.remoteConfig.fetchAndActivate().addOnCompleteListener {
+            val string = Firebase.remoteConfig.getString("test")
+            Log.e("!!!", "result = $string")
         }
     }
 
