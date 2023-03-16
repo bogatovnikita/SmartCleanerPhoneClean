6cb58746dc46afa75fd437dbb4d0e4fb3e4ab631
Nikita Bogatov <bogatovnikita@gmail.com>
2023-03-16 18:11:13 +0400

    premium: add layout

---
+++ premium/presentation/src/main/java/bogatovnikita/premium/presentation/ui/PremiumScreenFragment.kt
@@ -0,0 +1,8 @@
+package bogatovnikita.premium.presentation.ui
+
+import bogatovnikita.premium.presentation.base.BaseFragment
+import bogatovnikita.premium.presentation.databinding.FragmentPremiumScreenBinding
+
+class PremiumScreenFragment :
+    BaseFragment<FragmentPremiumScreenBinding>(FragmentPremiumScreenBinding::inflate) {
+}
