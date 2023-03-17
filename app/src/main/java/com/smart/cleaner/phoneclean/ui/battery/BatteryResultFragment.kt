4b602a9f4fd49a6042ec9da40c502fafda49353d
dimap <dima.pasyutin2@gmail.com>
2023-03-17 08:29:21 +0300

    app: complete reskin boost fun

--- app/src/main/java/com/smart/cleaner/phoneclean/ui/battery/BatteryResultFragment.kt
+++ app/src/main/java/com/smart/cleaner/phoneclean/ui/battery/BatteryResultFragment.kt
@@ -1,5 +1,8 @@
 package com.smart.cleaner.phoneclean.ui.battery
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.smart.cleaner.phoneclean.R
@@ -12,14 +15,24 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class BatteryResultFragment: BaseFragmentResult(R.layout.fragment_battery_result){
+class BatteryResultFragment : BaseFragmentResult(R.layout.fragment_battery_result) {
 
     private val binding: FragmentBatteryResultBinding by viewBinding()
 
     @Inject
     lateinit var resultList: ResultList
 
-    override fun setListFun(): List<FunResult> = resultList.getList().filter { it.type != OptimizingType.Battery }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnGoBack.setOnClickListener { findNavController().popBackStack() }
+    }
+
+    override fun setListFun(): List<FunResult> =
+        resultList.getList().filter { it.type != OptimizingType.Battery }
 
     override fun setRecyclerView(): RecyclerView = binding.recyclerView
 
--- app/src/main/java/com/smart/cleaner/phoneclean/ui/boost/BoostResultFragment.kt
+++ app/src/main/java/com/smart/cleaner/phoneclean/ui/boost/BoostResultFragment.kt
@@ -1,5 +1,10 @@
 package com.smart.cleaner.phoneclean.ui.boost
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.smart.cleaner.phoneclean.R
@@ -8,24 +13,50 @@ import com.smart.cleaner.phoneclean.ui.base.BaseFragmentResult
 import com.smart.cleaner.phoneclean.ui.result.FunResult
 import com.smart.cleaner.phoneclean.ui.result.ResultList
 import com.smart.cleaner.phoneclean.utils.OptimizingType
-import com.softcleean.fastcleaner.domain.boost.BoostUseCase
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class BoostResultFragment: BaseFragmentResult(R.layout.fragment_boost_result) {
+class BoostResultFragment : BaseFragmentResult(R.layout.fragment_boost_result) {
 
     private val binding: FragmentBoostResultBinding by viewBinding()
 
     @Inject
     lateinit var resultList: ResultList
 
-    @Inject
-     lateinit var boostUseCase: BoostUseCase
+    private val viewModel: BoostViewModel by viewModels()
 
-    override fun setListFun(): List<FunResult> = resultList.getList().filter { it.type != OptimizingType.Boost }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initScreenStateObserver()
+        initListeners()
+    }
+
+    private fun initScreenStateObserver() {
+        lifecycleScope.launch {
+            viewModel.screenState.collect { state ->
+                renderState(state)
+            }
+        }
+    }
+
+    private fun renderState(state: BoostScreenState) {
+        with(state) {
+            with(binding) {
+                circularProgressRamPercentDuplicate.progress = ramPercent.toFloat()
+                tvRamPercentsDuplicate.text = getString(R.string.value_percents, ramPercent)
+            }
+        }
+    }
+
+    private fun initListeners() {
+        binding.btnGoBack.setOnClickListener { findNavController().popBackStack() }
+    }
+
+    override fun setListFun(): List<FunResult> =
+        resultList.getList().filter { it.type != OptimizingType.Boost }
 
     override fun setRecyclerView(): RecyclerView = binding.recyclerView
-
 
 }
