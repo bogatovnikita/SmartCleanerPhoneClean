82a2e6b587a25ca8106deeb220aef6c886ad0766
dimap <dima.pasyutin2@gmail.com>
2023-03-21 18:18:11 +0300

    add module :duplicates:data and :duplicates:domain

--- duplicates/presentation/src/main/java/com/smart/cleaner/phoneclean/presentation/ui/duplicate_images/DuplicateImagesFragment.kt
+++ duplicates/presentation/src/main/java/com/smart/cleaner/phoneclean/presentation/ui/duplicate_images/DuplicateImagesFragment.kt
@@ -1,6 +1,12 @@
 package com.smart.cleaner.phoneclean.presentation.ui.duplicate_images
 
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.smart.cleaner.phoneclean.presentation.R
+import com.smart.cleaner.phoneclean.presentation.databinding.FragmentDuplicateImagesBinding
 
-class DuplicateImagesFragment : Fragment() {
+class DuplicateImagesFragment : Fragment(R.layout.fragment_duplicate_images) {
+
+    private val binding: FragmentDuplicateImagesBinding by viewBinding()
+
 }
