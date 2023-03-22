115cc1dfd4594d0d8872a533ced74e0c0b4d3815
dimap <dima.pasyutin2@gmail.com>
2023-03-22 19:32:45 +0300

    duplicates: presentation: complete render DuplicateFilesFragment

---
+++ duplicates/presentation/src/main/java/com/smart/cleaner/phoneclean/presentation/ui/duplicates_files/DuplicateFilesFragment.kt
@@ -0,0 +1,13 @@
+package com.smart.cleaner.phoneclean.presentation.ui.duplicates_files
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.smart.cleaner.phoneclean.presentation.R
+import com.smart.cleaner.phoneclean.presentation.databinding.FragmentDuplicateFilesBinding
+
+
+class DuplicateFilesFragment : Fragment(R.layout.fragment_duplicate_files) {
+
+    private val binding: FragmentDuplicateFilesBinding by viewBinding()
+
+}
