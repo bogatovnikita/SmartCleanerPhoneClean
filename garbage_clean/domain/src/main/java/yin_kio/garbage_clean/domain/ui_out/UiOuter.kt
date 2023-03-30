b37cef1dc18afb1ac260b9157f4f77677aa174b6
YinKio <jamycake@hotmail.com>
2023-03-30 11:25:10 +0300

    garbage_files.domain: add closePermissionDialog() use case

---
+++ garbage_clean/domain/src/main/java/yin_kio/garbage_clean/domain/ui_out/UiOuter.kt
@@ -0,0 +1,7 @@
+package yin_kio.garbage_clean.domain.ui_out
+
+interface UiOuter {
+
+    fun closePermissionDialog()
+
+}
--- garbage_clean/domain/src/test/kotlin/GarbageDistributorTest.kt
+++ garbage_clean/domain/src/test/kotlin/GarbageDistributorTest.kt
@@ -1,5 +1,3 @@
-package entities
-
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import yin_kio.garbage_clean.domain.garbage_files.GarbageFilesDistributor
