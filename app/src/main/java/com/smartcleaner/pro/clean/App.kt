f1670eabbcb778b08ccd4d97174a1ddb69d09b40
YinKio <jamycake@hotmail.com>
2023-04-12 14:40:14 +0300

    add app metrics

--- app/src/main/java/com/smartcleaner/pro/clean/App.kt
+++ app/src/main/java/com/smartcleaner/pro/clean/App.kt
@@ -1,7 +1,17 @@
 package com.smartcleaner.pro.clean
 
 import android.app.Application
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        val config = YandexMetricaConfig.newConfigBuilder(BuildConfig.APP_METRICS_KEY).build()
+        YandexMetrica.activate(this, config)
+        YandexMetrica.enableActivityAutoTracking(this)
+    }
+}
